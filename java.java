fff6ec76c4605a8acc562953799c81385055302a
664732047a <61307121+664732047a@users.noreply.github.com>
2020-06-29 16:41:36 +0800

    Update java.java

--- java.java
+++ java.java
@@ -1,5 +1,8 @@
 public class java{
   public static void main(String [] args){
     System.out.println("Hello World!!!");
+    for(int i = 0; i < 100 ; i++){
+      System.out.println("Hello World!");
+    }
   }
 }
